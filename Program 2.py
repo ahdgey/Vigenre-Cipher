2aecf13b882ca969122869531589c65955beba20
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-04-19 17:39:16 +0800

    Programmers name

--- Program 2.py
+++ Program 2.py
@@ -6,3 +6,7 @@
 print("\033[0;36m*" * 70)
 print("\033[1;97mThe Vigenère Cipher".center(77)) 
 print("\033[0;36m*" * 70)
+
+print("\033[1;32m\nHello! Your programmers name is Alexza Jean.")
+print("\033[1;32m\nShe's from BSCPE 1-4.")
+print("-" * 70)
