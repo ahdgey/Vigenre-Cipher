c07cdc94c655b8b4a7b050bbc93ec5fd045856ab
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-04-19 18:07:22 +0800

    Design

--- Program 2.py
+++ Program 2.py
@@ -23,3 +23,5 @@ ciphertext = ""
 for i in range(len(message)):
     encrypted_num = (ord(message[i]) - 65 + key_map[i % len(key_map)]) % 26
     ciphertext += chr(encrypted_num + 65)
+
+print("=" * 70)
