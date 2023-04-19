e6abeaeed6ef024b925a1e2cc909b7974f83ace1
Alexza Jean Catanoy <alexzacatanoy@gmail.com>
2023-04-19 17:56:38 +0800

    Request input

--- Program 2.py
+++ Program 2.py
@@ -10,3 +10,7 @@ print("\033[0;36m*" * 70)
 print("\033[1;32m\nHello! Your programmers name is Alexza Jean.")
 print("\033[1;32m\nShe's from BSCPE 1-4.")
 print("-" * 70)
+
+# Request for users message and key input
+message = input("\033[0;33m\nKindly type in the message(all uppercase letters, no spaces): ")
+key = input("\033[0;33m\nKindly type in the keyword(all uppercase letters): ")
